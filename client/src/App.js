22c8c91ac0ceceebc54da2582efbd91b9f1c3e02
thebrijpatel <sadariyabrijesh@gmail.com>
2021-03-25 09:17:17 +0530

    Adding the uploader

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import FileUpload from '../src/components/FileUpload/FileUpload';
+
+const App = () => (
+    <div className="container mt-4">
+     <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react" /> React File Upload 
+     </h4>
+
+     <FileUpload />
+
+    </div>
+);
+
+export default App;
